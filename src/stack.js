def9f88f4df4c5b09b1de1e17af96f4219cdb966
unknown <james.pamp@hotmail.com>
2020-05-24 15:02:12 +0200

    Small changes to stack.js file

---
+++ src/stack.js
@@ -0,0 +1,18 @@
+var _ = require('underscore');
+
+let stack = [];
+
+//Lägger ett element överst i stacken
+exports.push = (x) => {
+  stack.push(x);
+};
+
+//Returnerar det översta elementet i stacken och tar bort det
+exports.pop = () => {
+  return stack.pop();
+};
+
+//Returnerar det översta elementet i stacken
+exports.peek = () => {
+  return stack[0] 
+}
